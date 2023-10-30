c2bc35e397767b9c40d21ee4e1d6c9c3549c6524
forhadulislam <shifat2sadi@gmail.com>
2023-10-31 01:09:41 +0200

    change Dockerfile

--- services/klas/main.go
+++ services/klas/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/forhadulislam/golms/packages/config"
 )
 
 func main() {
-	fmt.Println("My new Service empty!") 
+	fmt.Println("My new Service empty!")
+
+	fmt.Println(config.Config())
 }
